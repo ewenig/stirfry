a0fe2fec1ad1e2168558b328cab38fbb578e1406
Anthony Gargiulo <agargiulo@anthonygargiulo.info>
2011-07-20 23:02:07 -0400

    fixed a few hanging refs

--- stir-fry.sh
+++ stir-fry.sh
@@ -2,10 +2,10 @@
 #Remember to change the $STIRFRY_DIR first
 $STIRFRY_DIR='/path/to/sitrfry/'
 
-if [ -n "$STIRFRY_DIR" ]; then
+if [ -d "$STIRFRY_DIR" ]; then
    cd $STIRFRY_DIR
    wget -O gracies-weekly.cfm http://finweb.rit.edu/diningservices/forms/webmenus/gracies-weekly.cfm
    perl stir-fry.pl > ~/public_html/stirfry/index.htm
 else
-   echo "Could not change directory - did you export \$STIRFRY_DIR?"
+   echo "Could not change directory - did you change \$STIRFRY_DIR?"
 fi
